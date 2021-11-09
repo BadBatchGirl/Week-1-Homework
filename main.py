58b72a6a5d536bf54957d102a1644b29921adabb
BadBatchGirl <94010537+BadBatchGirl@users.noreply.github.com>
2021-11-09 13:33:26 -0500

    Complete solution

---
+++ main.py
@@ -0,0 +1,7 @@
+adjective = "pink"
+noun = "Skittle"
+past_tense_verb = "smiled"
+adverb = "happily"
+verb = "stretch"
+
+print(f"Today I went to the zoo. I saw a {adjective} {noun} jumping up and down in its tree. He {past_tense_verb} {adverb} through the large tunnel that led to its {adjective} {noun}. I got peanuts and passed them through the cage to a gigantic gray {noun} towering above my head. Feeding that animal made me hungry. I went to get a {adjective} scoop of ice cream. It filled my stomach. Afterwards I had to {verb} {adverb} to catch our bus. When I got home I {past_tense_verb} my mom for a {adjective} day at the zoo.")
